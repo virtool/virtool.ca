9626a5ca7ce9931f78e6cd170e697c6a302ae69c
Ian Boyes <igboyes@gmail.com>
2022-01-17 18:05:41 -0800

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    title: `virtool.ca`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `src`,
+        path: `${__dirname}/src/`,
+      },
+    },
+  ],
+};
