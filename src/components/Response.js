961d4f1c23825c74ae37e7d40ce13b7f718828d9
elanjm12 <97321944+elanjm12@users.noreply.github.com>
2022-08-15 13:02:23 -0400

    feat: auto-generate docs from openapi json

--- src/components/Response.js
+++ src/components/Response.js
@@ -3,6 +3,6 @@ import React from "react";
 export const Response = ({ children, status }) => (
   <div className="border-solid border-2 border-stone-700">
     <p className="p-4 text-md">Status: {status}</p>
-    {children}
+    <pre id="json">{children}</pre>
   </div>
 );
