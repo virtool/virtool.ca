59dcbe2cb993b29d6e0635c8addab8c2715b363b
Ian Boyes <igboyes@gmail.com>
2022-08-30 15:56:58 -0700

    fix: restore navigation and remove dup headers

--- src/pages/docs/api.js
+++ src/pages/docs/api.js
@@ -1,12 +1,4 @@
-import {
-  capitalize,
-  isEqual,
-  keysIn,
-  map,
-  nth,
-  split,
-  unionWith,
-} from "lodash";
+import { map, nth, split, uniq } from "lodash";
 import React from "react";
 import ApiEndpoint from "../../components/ApiEndpoint";
 import { Link } from "../../components/Link";
@@ -14,38 +6,45 @@ import { Main } from "../../components/Main";
 import { Nav } from "../../components/Nav";
 import json from "../../data/openapi.json";
 
-const API = () => {
-  let apiLinkTitles = [];
-
-  map(keysIn(json.paths), (endpoint) => {
-    const formattedApiTitle = capitalize(nth(split(endpoint, "/"), 1));
-    apiLinkTitles = unionWith(apiLinkTitles, [formattedApiTitle], isEqual);
-  });
-
-  const apiTitles = [...apiLinkTitles];
-
-  const apiDocs = map(keysIn(json.paths), (endpoint) => {
-    console.log("endpoint", endpoint);
-
-    const formattedEndpoint = capitalize(nth(split(endpoint, "/"), 1));
-
-    return map(keysIn(json.paths[endpoint]), (method) => {
-      return (
-        <div>
-          {formattedEndpoint === apiTitles[0] ? (
-            <h1 className="col-span-2 text-4xl font-bold mb-5">
-              {apiTitles.shift()}
-            </h1>
-          ) : null}
+const APISection = ({ endpoints, path }) => (
+  <div>
+    <div className="divide-y-2 divide-dashed divide-slate-300">
+      {map(endpoints, (endpoint, method) => {
+        return (
           <ApiEndpoint
-            key={method + endpoint}
-            endpoint={endpoint}
+            key={method + path}
             method={method}
-            data={json.paths[endpoint][method]}
+            path={path}
+            endpoint={endpoint}
           />
-        </div>
-      );
-    });
+        );
+      })}
+    </div>
+  </div>
+);
+
+const API = () => {
+  let titles = [];
+
+  const apiDocs = map(json.paths, (endpoints, path) => {
+    const title = nth(split(path, "/"), 1);
+    const show = !titles.includes(title);
+
+    titles.push(title);
+
+    return (
+      <>
+        {show && (
+          <h1
+            id={title}
+            className="capitalize col-span-2 font-bold mb-5 scroll-mt-24 text-4xl"
+          >
+            {title}
+          </h1>
+        )}
+        <APISection key={path} path={path} endpoints={endpoints} />
+      </>
+    );
   });
 
   return (
@@ -53,13 +52,13 @@ const API = () => {
       <Nav />
       <div className="lg:container mx-auto gap-2 grid grid-cols-9">
         <div className="col-span-1 sticky top-20 self-start">
-          {map(apiLinkTitles, (title) => (
-            <div key={title}>
+          {map(uniq(titles), (title) => (
+            <div key={title} className="capitalize">
               <Link to={`#${title}`}>{title}</Link>
             </div>
           ))}
         </div>
-        <div className="col-span-8 bg-neutral-50 font-sans pt-10 divide-y-2 divide-dashed divide-slate-300">
+        <div className="col-span-8 bg-neutral-50 font-sans pt-10">
           {apiDocs}
         </div>
       </div>
