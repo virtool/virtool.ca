ed2ec8280334da04c59dbc3d192e813fbe2a59c5
Ian Boyes <igboyes@gmail.com>
2022-08-30 09:54:32 -0700

    fix: make api methods uppercase

--- src/components/Endpoint.js
+++ src/components/Endpoint.js
@@ -2,7 +2,9 @@ import React from "react";
 
 export const Endpoint = ({ method, path }) => (
   <div className="border-purple-700 border-solid border-2 flex items-center gap-4 text-lg font-mono my-4">
-    <div className="py-2 px-5 bg-purple-700 text-white font-bold">{method}</div>
+    <div className="py-2 px-5 bg-purple-700 text-white font-bold uppercase">
+      {method}
+    </div>
     <div className="">{path}</div>
   </div>
 );
