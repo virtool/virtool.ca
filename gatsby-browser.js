96793bbf7007e83b0af26b57f3248f487aaff91d
Ian Boyes <igboyes@gmail.com>
2022-09-14 15:13:57 -0700

    fix: use correct components in mdx rendering

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,14 +8,14 @@ import React from "react";
 import { Administrator } from "./src/components/Administrator";
 import { AdministratorOrOwner } from "./src/components/AdministratorOrOwner";
 import { Endpoint } from "./src/components/Endpoint";
-import * as Headings from "./src/components/Headings";
+import { H1, H2, H3, P } from "./src/components/Headings";
 import { PrimaryLayout } from "./src/components/Layout";
 import { Permission } from "./src/components/Permission";
 import { Recommended } from "./src/components/Recommended";
 import { Request } from "./src/components/Request";
 import { Response } from "./src/components/Response";
 import { Status } from "./src/components/Status";
-import * as Table from "./src/components/Table";
+import { Table, Td, Th, Tr } from "./src/components/Table";
 import { Warning } from "./src/components/Warning";
 
 const components = {
@@ -24,14 +24,20 @@ const components = {
   Administrator,
   AdministratorOrOwner,
   Endpoint,
+  h1: H1,
+  h2: H2,
+  h3: H3,
   Response,
   Warning,
   Recommended,
+  p: P,
   Permission,
   PrimaryLayout,
   Status,
-  ...Headings,
-  ...Table,
+  table: Table,
+  td: Td,
+  th: Th,
+  tr: Tr,
 };
 
 export const wrapRootElement = ({ element }) => (
