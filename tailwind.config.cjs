6a7cbd1d83de805525ccb0f2a0a46a31cfc9126e
Ian Boyes <igboyes@gmail.com>
2023-05-11 16:51:24 -0700

    fix: remove backticks from inline code
    fix: remove code backticks in tailwind theme

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,7 +2,16 @@
 module.exports = {
   content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {},
+    extend: {
+      typography: {
+        DEFAULT: {
+          css: {
+            "code::before": { content: "" },
+            "code::after": { content: "" },
+          },
+        },
+      },
+    },
   },
   plugins: [require("@tailwindcss/typography")],
 };
